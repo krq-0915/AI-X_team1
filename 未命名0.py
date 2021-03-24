35aa25853710366d9f6df12fe413dc90b6bd24a8
krq-0915 <2967196409@qq.com>
2021-03-24 10:31:10 +0800

    origin

---
+++ 未命名0.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 16:17:39 2021
+
+@author: ruiqi
+"""
+
+import torch
+
+x_data=torch.Tensor([[1.0],[2.0],[3.0]])
+y_data=torch.Tensor([[2.0],[4.0],[6.0]])
+
+class LinearModel(torch.nn.Module):
+    def __init__(self):
+        super(LinearModel,self).__init__()
+        self.linear = torch.nn.Linear(1,1)
+        
+    def forward(self,x):
+        y_pred = self.linear(x)
+        return y_pred
+
+model = LinearModel()
+criterior = torch.nn.MSELoss(size_average=False)
+optimizer = torch.optim.SGD(model.parameters(),lr=0.01)
+
+for epoch in range(5000):
+    y_pred = model(x_data)
+    loss = criterior(y_pred,y_data)
+    print(epoch,loss)
+    
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+print('w = ',model.linear.weight.item())
+print('b = ',model.linear.bias.item())
+
+
+
+
+
+
